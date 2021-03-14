8cc8ba72273b77e2c264b37d930cf451a535bb12
AffanTheBest <affan207278@gmail.com>
2021-03-14 17:24:30 +0530

    ...

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const fetchUrl = require('fetch').fetchUrl;
 
-const key = 'f250c5003a6b2bfdaaa146fef33fa60bb08ec';
-const short = 'https://compstreasure.cf';
+// const key = 'f250c5003a6b2bfdaaa146fef33fa60bb08ec';
+// const short = 'https://compstreasure.cf';
 
-const shorting = (link) => {
-    return fetchUrl(`http://cutt.ly/api/api.php?key=f250c5003a6b2bfdaaa146fef33fa60bb08ec&short=${link}` , (err , res , body) => {
-        var realData = JSON.parse(body);
-        // console.log(realData.url.shortLink);
-        return realData.url.shortLink;
-    })
-}
+// const shorting = (link) => {
+//     return fetchUrl(`http://cutt.ly/api/api.php?key=f250c5003a6b2bfdaaa146fef33fa60bb08ec&short=${link}` , (err , res , body) => {
+//         var realData = JSON.parse(body);
+//         // console.log(realData.url.shortLink);
+//         return realData.url.shortLink;
+//     })
+// }
 
-console.log(shorting(short));
+// console.log(shorting(short));
 
 const { Telegraf } = require('telegraf');
 const bot = new Telegraf(process.env.BPT_TOKEN || '1655688053:AAGOtNRzLKGye7GCou6TXmGIlrE6WAaoyUA');
