212f5fa436ea6152121c238d2d7f86ce2701d896
AffanTheBest <affan207278@gmail.com>
2021-03-15 19:16:16 +0530

    Added support to unshort links

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
 const axios = require('axios');
 const validUrl = require('valid-url');
-
 const { Telegraf } = require('telegraf');
-const bot = new Telegraf(process.env.BOT_TOKEN);
-const key = process.env.API_KEY;
+const short = require('./modules/short');
+const unshort = require('./modules/unshort');
+const {token } = require('./config');
+
+const bot = new Telegraf(token);
 
 bot.start((ctx) => ctx.replyWithMarkdown(`Hey [${ctx.message.chat.first_name}](https://t.me/${ctx.message.chat.username}) , Welcome !\nSend me a long URL and get it shortened. \n\n Made with ❤ by [𝔄𝔉𝔉𝔄𝔑](https://t.me/AffanTheBest) .`));
-bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.help((ctx) => ctx.replyWithMarkdown("/start - Restart the bot. \n/help - Get this message. \n\n/unshort - Extract long URL from any shortend URL. (Eg.  ``` /unshort https://cutt.ly/qzXU9A2```). \n\nTo short a big URL just send me the long URL and I'll give shorten link."))
 bot.on('sticker', (ctx) => ctx.reply('Am I joke to you , Send an url to get work done!'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there!'))
+bot.command('unshort' , (ctx) => unshort(ctx.message.text , ctx.chat.id))
 bot.on('text', (ctx) => {
     if(validUrl.isUri(ctx.message.text)){
-        axios.get(`http://cutt.ly/api/api.php?key=${key}&short=${ctx.message.text}`)
-        .then((res) => {
-            const shortenURL = res.data.url.shortLink;
-        bot.telegram.sendMessage(ctx.chat.id ,`Here's the shorten url : \n👉 ${shortenURL}` , 
-        {
-            reply_markup: {
-                inline_keyboard: [ [{text: shortenURL , url:shortenURL}] ]
-        }})})
-        .catch(err => ctx.reply('Sorry, an error has been occurred. PLease try again later.'))
+        short(ctx.message.text , ctx.chat.id);
     }else{
         ctx.reply('Please send a valid URL !');
     }})
+
 
 bot.launch()
 
