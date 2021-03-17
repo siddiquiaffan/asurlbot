14c93f42347f88830d93ff834faef3fd5dcf91a7
AffanTheBest <affan207278@gmail.com>
2021-03-17 17:55:16 +0530

    Added short command

--- index.js
+++ index.js
@@ -7,19 +7,32 @@ const {token } = require('./config');
 
 const bot = new Telegraf(token);
 
-bot.start((ctx) => ctx.replyWithMarkdown(`Hey [${ctx.message.chat.first_name}](https://t.me/${ctx.message.chat.username}) , Welcome !\nSend me a long URL and get it shortened. \n\n Made with ❤ by [𝔄𝔉𝔉𝔄𝔑](https://t.me/AffanTheBest) .`));
+bot.start((ctx) => {
+    if(ctx.message.chat.type == 'private'){
+        console.log("It's private chat.")
+        console.log(ctx.message)
+    }else{
+        console.log(ctx.message);
+    }
+});
 bot.help((ctx) => ctx.replyWithMarkdown("/start - Restart the bot. \n/help - Get this message. \n\n/unshort - Extract long URL from any shortend URL. (Eg.  ``` /unshort https://cutt.ly/qzXU9A2```). \n\nTo short a big URL just send me the long URL and I'll give shorten link."))
 bot.on('sticker', (ctx) => ctx.reply('Am I joke to you , Send an url to get work done!'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there!'))
-bot.command('unshort' , (ctx) => unshort(ctx.message.text , ctx.chat.id))
-bot.on('text', (ctx) => {
-    if(validUrl.isUri(ctx.message.text)){
-        short(ctx.message.text , ctx.chat.id);
+bot.command('unshort' , (ctx) => unshort(ctx.message.text , ctx.chat.id));
+bot.command('short' , (ctx) => {
+    const url = ctx.message.text.split(' ').slice(1)[0];
+    if(validUrl.isUri(url)){
+        short(url , ctx.chat.id);
     }else{
         ctx.reply('Please send a valid URL !');
+    }
+})
+bot.on('text', (ctx) => {
+    if(ctx.message.chat.type == 'private' && validUrl.isUri(ctx.message.text)){
+        short(ctx.message.text , ctx.chat.id, ctx.message.from.username);
+    }else if(ctx.message.chat.type == 'private'){
+        ctx.reply('Please send a valid URL !');
     }})
-
-
 bot.launch()
 
 // Enable graceful stop
