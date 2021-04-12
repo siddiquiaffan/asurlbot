9fbef9a5815c975893c615f3f0f80ddba1b9182e
AffanTheBest <siddiquiaffan207278@gmail.com>
2021-04-12 15:15:06 +0530

    improved

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const key = process.env.API_KEY || "f250c5003a6b2bfdaaa146fef33fa60bb08ec";
-const token = process.env.BOT_TOKEN || "1645764818:AAHCJUfF-j8F2joeQp2ifKO4QRKPENNvs_c";
+const key = process.env.API_KEY;
+const token = process.env.BOT_TOKEN;
 
 module.exports = {key , token};
