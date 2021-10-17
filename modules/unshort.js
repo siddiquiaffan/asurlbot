6688f2c04b662bc6d740cbe835adc4548b80d3fa
AffanTheBest <siddiquiaffan207278@gmail.com>
2021-10-17 12:33:24 +0530

    Update unshort

--- modules/unshort.js
+++ modules/unshort.js
@@ -1,13 +1,20 @@
 const axios = require("axios");
 
-const unshort = async (url) => {
-      const res = await axios.get(url);
-      try{
-            const longUrl = res.request.res.responseUrl ? res.request.res.responseUrl : url;
-            return longUrl;
-      }catch(err){
-            return 'Failed to extratc long url.'
-      }
-   
+const unshort = async(url) => {
+      const extractUrl = req => req?.request?.res?.responseUrl || 
+      req?.request?._redirectable?._currentUrl ||  
+      req?.request?._currentUrl ||  
+      req?.request?._options?.href || 
+      'https://'+ req?.request?.host + req?.request?.path;
+    
+        try{
+            const req = await axios.get(url);
+            const result = extractUrl(req);
+            var longUrl = result ? result : url;
+        }catch(err){
+            const result = extractUrl(err);
+            var longUrl = result ? result : url;
+        }
+      return longUrl;
 }
 module.exports = unshort;
