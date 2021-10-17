a6c2b100d45fbfc490585b7c23b5d6e0bf814b1f
AffanTheBest <siddiquiaffan207278@gmail.com>
2021-10-17 12:33:51 +0530

    Export directly

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const key = process.env.API_KEY || 'f250c5003a6b2bfdaaa146fef33fa60bb08ec';
-const token = process.env.BOT_TOKEN || '1789234611:AAEaPJ1Yyo3MS_Jg_ytcTGdvYoFzFMUiEfU';
-
-module.exports = {key , token};
+module.exports = {
+    key : process.env.API_KEY || 'f250c5003a6b2bfdaaa146fef33fa60bb08ec',
+    token : process.env.BOT_TOKEN || '1645764818:AAHCJUfF-j8F2joeQp2ifKO4QRKPENNvs_c'
+};
